236e1e75c3307fe2cdb76703861e4c2d325a578f
Ubludor <90902613+Ubludor@users.noreply.github.com>
2024-04-06 15:44:19 +0300

    first commit

---
+++ init_db/init.sql
@@ -0,0 +1,12 @@
+--- init.sql
+CREATE TABLE EMPLOYEE (
+  empId INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  dept TEXT NOT NULL
+);
+
+-- insert
+INSERT INTO EMPLOYEE VALUES (0001, 'Clark', 'Sales');
+INSERT INTO EMPLOYEE VALUES (0002, 'Dave', 'Accounting');
+INSERT INTO EMPLOYEE VALUES (0003, 'Ava', 'Sales');
+
